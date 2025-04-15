27e60f083c782d5249bb95c1b5ce10422e076b36
Alfarooq Almenghawi <a.almenghawi123@gmail.com>
2025-04-15 15:45:13 +0100

    Intialize basic react routes to prepare for functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { useState } from "react";
-import Header from "./components/Header/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header.jsx";
+import BrowseEvents from "./components/BrowseEvents/BrowseEvents.jsx";
+import MyEvents from "./components/MyEvents/MyEvents.jsx";
+import Login from "./components/Login/Login.jsx";
+import SignUp from "./components/SignUp/SignUp.jsx";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-    </>
+      <Routes>
+        <Route path="/browse-events" element={<BrowseEvents />} />
+        <Route path="/my-events" element={<MyEvents />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/MyEvents/MyEvents.jsx
@@ -0,0 +1,9 @@
+const MyEvents = () => {
+  return (
+    <div>
+      <h1>My Events</h1>
+      {/* Add your my events logic here */}
+    </div>
+  );
+};
+export default MyEvents;
