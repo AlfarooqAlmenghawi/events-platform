6ef1894c664c9bb5188abc5740c4c8aa83ea6a64
Alfarooq Almenghawi <a.almenghawi123@gmail.com>
2025-04-15 15:45:13 +0100

    Intialize basic react routes to prepare for functionality

---
+++ src/components/BrowseEvents/BrowseEvents.jsx
@@ -0,0 +1,9 @@
+const BrowseEvents = () => {
+  return (
+    <div>
+      <h1>Browse Events</h1>
+      {/* Add your event browsing logic here */}
+    </div>
+  );
+};
+export default BrowseEvents;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,25 @@
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
   return (
-    <>
-      <header className="header">
-        <h1>Events Platform</h1>
-        <section className="header-buttons">
+    <header className="header">
+      <h1>Events Platform</h1>
+      <section className="header-buttons">
+        <Link to="/browse-events">
           <button>Browse Events</button>
+        </Link>
+        <Link to="/my-events">
           <button>My Events</button>
+        </Link>
+        <Link to="/login">
           <button>Login</button>
+        </Link>
+        <Link to="/signup">
           <button>Sign Up</button>
-        </section>
-      </header>
-    </>
+        </Link>
+      </section>
+    </header>
   );
 };
 
